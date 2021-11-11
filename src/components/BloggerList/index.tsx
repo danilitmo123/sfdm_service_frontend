4cd51819df5351c502777cf973a482772bf5f9c8
danilitmo123 <danilitmo@gmail.com>
2021-11-11 17:34:30 +0300

    update fetchBloggers

--- src/components/BloggerList/index.tsx
+++ src/components/BloggerList/index.tsx
@@ -2,14 +2,22 @@ import React, { FC } from 'react';
 
 import './BloggerList.scss';
 import { bloggersAPI } from '../../services/BloggerService';
+import BloggerItem from './BloggerItem';
+import { IBlogger } from '../../models/IBlogger';
 
 const BloggerList: FC = () => {
-  const { data: bloggers } = bloggersAPI.useFetchAllBloggersQuery('');
+  const { data: bloggers, error, isLoading } = bloggersAPI.useFetchAllBloggersQuery('');
 
-  console.log(bloggers);
+  console.log(bloggers, isLoading);
+
+  if (isLoading) {
+    return <h1>Идет загрузка</h1>;
+  }
+
   return (
     <div className="blogger-list">
-      {/* {mockBloggers.map((blogger) => <BloggerItem blogger={blogger} key={blogger.id} />)} */}
+      {error && <h1>Ошибка</h1>}
+      {bloggers && bloggers.map((blogger: IBlogger) => <BloggerItem blogger={blogger} key={blogger.id} />)}
     </div>
   );
 };
